3fab8c0fd49b122a574a31c22c045f9fd4c98a74
xhs <chef@dark.kitchen>
2015-04-12 12:58:02 +0800

    guess data format

--- gateway/lib/handler/socket.ex
+++ gateway/lib/handler/socket.ex
@@ -6,12 +6,42 @@ defmodule Handler.Socket do
   fields socket: nil,
          ip_address: nil
 
-  def sockjs_init(connection, :json_interface) do
+  def sockjs_init(connection, []) do
     [{:headers, headers}] = Enum.filter connection.info, fn({k, _v}) -> k == :headers end
     [{:'x-real-ip', ip_address}] = Enum.filter headers, fn({k, _v}) -> k == :'x-real-ip' end
     Lager.info "[handler.socket/sockjs_init] socket ip address: ~p", [ip_address]
     socket_info = new socket: self, ip_address: ip_address
-    {:ok, {:json, socket_info}}
+    {:ok, {:unknown, socket_info}}
+  end
+
+  def sockjs_handle(connection, data, {:unknown, socket_info}) do
+    try do
+      list = :jsx.decode data
+      try do
+        action = :proplists.get_value "action", list
+        true = action != :undefined
+        do_handle connection, action, list, {:json, socket_info}
+      rescue
+        _whatever ->
+          Lager.info "[handler.socket/sockjs_handle] bad json request"
+          body = encode_body :json, "error", "bad.request", :null
+          connection.send body
+          {:ok, {:json, socket_info}}
+      end
+    rescue
+      _whatever ->
+        try do
+          map = Bson.decode data
+          action = map.action
+          do_handle connection, action, map, {:bson, socket_info}
+        rescue
+          _whatever ->
+            Lager.info "[handler.socket/sockjs_handle] bad bson request"
+            body = encode_body :bson, "error", "bad.request", :null
+            connection.send body
+            {:ok, {:bson, socket_info}}
+        end
+    end
   end
 
   def sockjs_handle(connection, json, state={:json, socket_info}) do
@@ -20,11 +50,10 @@ defmodule Handler.Socket do
       list = :jsx.decode json
       action = :proplists.get_value "action", list
       true = action != :undefined
-      Lager.debug "[handler.socket/sockjs_handle] request action: ~p", [action]
       do_handle connection, action, list, state
     rescue
       _whatever ->
-        Lager.info "[handler.socket/sockjs_handle] bad request"
+        Lager.info "[handler.socket/sockjs_handle] bad json request"
         body = encode_body :json, "error", "bad.request", :null
         connection.send body
         {:ok, state}
@@ -36,11 +65,10 @@ defmodule Handler.Socket do
     try do
       map = Bson.decode bson
       action = map.action
-      Lager.debug "[handler.socket/sockjs_handle] request action: ~p", [action]
       do_handle connection, action, map, state
     rescue
       _whatever ->
-        Lager.info "[handler.socket/sockjs_handle] bad request"
+        Lager.info "[handler.socket/sockjs_handle] bad bson request"
         body = encode_body :bson, "error", "bad.request", :null
         connection.send body
         {:ok, state}
@@ -48,6 +76,7 @@ defmodule Handler.Socket do
   end
 
   defp do_handle(connection, action, _collection, state={interface, _}) do
+    Lager.debug "[handler.socket/do_handle] request action: ~p", [action]
     body = encode_body interface, "error", "unsupported.action", action
     connection.send body
     {:ok, state}
